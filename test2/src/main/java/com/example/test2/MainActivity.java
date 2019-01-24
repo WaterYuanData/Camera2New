58f556d06fa58e117f971273fe627ccc274c94ce
water <water.yuan@qq.com>
2019-01-25 00:53:55 +0800

    只依赖 aar文件

--- test2/src/main/java/com/example/test2/MainActivity.java
+++ test2/src/main/java/com/example/test2/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.test2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.aarlib.AarUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AarUtils.log(getApplicationContext());
     }
 }
