0cdb7299858baed30bc71e7cd90c5adae173498f
water <water.yuan@qq.com>
2019-01-29 00:53:16 +0800

    配置Beta版

--- app/src/main/java/com/example/ran/testaar/MainActivity.java
+++ app/src/main/java/com/example/ran/testaar/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.ran.testaar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "yyyy";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean log_debug = BuildConfig.LOG_DEBUG;
+        Log.d(TAG, "onCreate: log_debug="+log_debug);
     }
 }
---
+++ build.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+echo '555'
+
+./gradlew assembleBeta33
