abe093ef9902b069600a29a4733050184ed7dbf7
water <water.yuan@qq.com>
2019-03-20 00:34:11 +0800

    动态加载

--- testforview/src/main/java/com/example/yuan/testforview/MyFragmentActivity.java
+++ testforview/src/main/java/com/example/yuan/testforview/MyFragmentActivity.java
@@ -9,5 +9,8 @@ public class MyFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_fragment);
+
+        MyFragment myFragment = new MyFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment2, myFragment).commit();
     }
 }
