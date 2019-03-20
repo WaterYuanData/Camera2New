d564063bf4680875bd49628dfef9afc53732d448
water <water.yuan@qq.com>
2019-03-21 00:13:05 +0800

    生命周期

--- testforview/src/main/java/com/example/yuan/testforview/MyFragment.java
+++ testforview/src/main/java/com/example/yuan/testforview/MyFragment.java
@@ -1,18 +1,88 @@
 package com.example.yuan.testforview;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MyFragment extends Fragment {
+    private static final String TAG = "MyFragment";
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: ");
         return inflater.inflate(R.layout.fragment_my, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated: ");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated: ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach: ");
+    }
+
 }
--- testforview/src/main/java/com/example/yuan/testforview/MyFragmentActivity.java
+++ testforview/src/main/java/com/example/yuan/testforview/MyFragmentActivity.java
@@ -2,15 +2,44 @@ package com.example.yuan.testforview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MyFragmentActivity extends AppCompatActivity {
+    private static final String TAG = "MyFragmentActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_my_fragment);
-
+        Log.i(TAG, "onCreate: ------");
         MyFragment myFragment = new MyFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment2, myFragment).commit();
+        Log.d(TAG, "onCreate: ++++++");
     }
+    /*
+    只静态加载
+03-20 16:10:36.561 28123-28123/com.example.yuan.testforview D/MyFragmentActivity: onCreate:
+03-20 16:10:36.653 28123-28123/com.example.yuan.testforview D/MyFragment: onAttach:
+    onCreate:
+03-20 16:10:36.654 28123-28123/com.example.yuan.testforview D/MyFragment: onCreateView:
+03-20 16:10:36.661 28123-28123/com.example.yuan.testforview D/MyFragment: onViewCreated:
+03-20 16:10:36.661 28123-28123/com.example.yuan.testforview I/MyFragmentActivity: onCreate: ------
+03-20 16:10:36.661 28123-28123/com.example.yuan.testforview D/MyFragmentActivity: onCreate: ++++++
+03-20 16:10:36.664 28123-28123/com.example.yuan.testforview D/MyFragment: onActivityCreated:
+    onStart:
+03-20 16:10:36.667 28123-28123/com.example.yuan.testforview D/MyFragment: onResume:
+
+    只动态加载
+03-20 16:08:20.691 27953-27953/com.example.yuan.testforview D/MyFragmentActivity: onCreate:
+03-20 16:08:20.739 27953-27953/com.example.yuan.testforview I/MyFragmentActivity: onCreate: ------
+03-20 16:08:20.755 27953-27953/com.example.yuan.testforview D/MyFragmentActivity: onCreate: ++++++
+03-20 16:08:20.765 27953-27953/com.example.yuan.testforview D/MyFragment: onAttach:
+    onCreate:
+03-20 16:08:20.766 27953-27953/com.example.yuan.testforview D/MyFragment: onCreateView:
+03-20 16:08:20.772 27953-27953/com.example.yuan.testforview D/MyFragment: onViewCreated:
+    onActivityCreated:
+    onStart:
+03-20 16:08:20.774 27953-27953/com.example.yuan.testforview D/MyFragment: onResume:
+    */
 }
