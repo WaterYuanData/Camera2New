442b8351e581c6044fc7f88e2b9b72210813c606
WaterYuan <water.yuan.data@gmail.com>
2019-04-24 17:38:59 +0800

    动态加载Fragment，注意xml及attachToRoot

--- camera2video/src/main/java/com/example/camera2video/CameraFragment.java
+++ camera2video/src/main/java/com/example/camera2video/CameraFragment.java
@@ -1,6 +1,19 @@
 package com.example.camera2video;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CameraFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//        attachToRoot 默认是true
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
 }
